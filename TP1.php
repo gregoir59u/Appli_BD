44af023b732b12fd160c6638cabed3ed570ea0f9
Maxime Brasley <75072652+maxime-brsl@users.noreply.github.com>
2022-03-28 14:56:47 +0200

    udapte

--- TP1.php
+++ TP1.php
@@ -50,3 +50,15 @@ foreach ($games as $game) {
     $numberq4 += 1;
 }
 echo "<br><h3>Nombre de lignes : $numberq4</h3>";
+
+/*Question 5)
+$numberq5 = 0;
+$games = Game::select('name', 'deck')->paginate(15)->get();
+echo '<h2>Q5) Lister les jeux, afficher leur nom et deck, en paginant (taille des pages : 500)</h2>';
+$games = Game::simplePaginate(15);
+echo $games;
+foreach ($games as $game) {
+    echo '<br>JEU : ' . $game->name . "<br>DECK : " . $game->deck . "<br>";
+    $numberq5 += 1;
+}
+echo "<br><h3>Nombre de lignes : $numberq5</h3>";*/
